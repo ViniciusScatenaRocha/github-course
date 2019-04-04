6543406cb61c4135515f0a92b20af5a54b4cc92a
Vinicius Scatena Rocha <vinicius.scatena@birmind.com.br>
2019-04-04 11:19:23 -0300

    alteracoes grafico

--- simplemoc/core/views.py
+++ simplemoc/core/views.py
@@ -70,9 +70,9 @@ def gerar_graf():
     line_chart = pygal.Line()
     line_chart.title = 'Numero de restaurantes (em %)'
     line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Hamburgueria BoiNaChapa', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Pastelaria Cruzoé', [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
+    line_chart.add('Hamburgueria', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add('Pastelaria', [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
     line_chart.add('Newton Massas', [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Pizzaria do Nono', [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
+    line_chart.add('Pizzaria', [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
 
     return line_chart.render()
